5445373ba67d5a5e8008d9e438d06539e1545487
Владислав Урдаев <VUrdaev-KI20@stud.sfu-kras.ru>
2023-10-18 09:42:10 +0000

    Добавил конфигурацию для EF Core

---
+++ EmploySity/Data/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace EmploySity.Data.Interfaces;
+
+public interface IRepository<TEntity>
+{
+    Task<List<TEntity>> GetAllAsync();
+    
+    Task<TEntity?> GetByIdAsync(Guid id);
+
+    IQueryable<TEntity> GetQueryable();
+
+    void Insert(TEntity entity);
+    
+    void Update(TEntity entity);
+    
+    void Remove(TEntity entity);
+
+    Task SaveChangesAsync();
+}
--- EmploySity/Program.cs
+++ EmploySity/Program.cs
@@ -1,27 +1,34 @@
+using EmploySity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
+app.MapControllers();
 
 app.Run();
